0e3b217231d6eb89662cc2217801962bda751134
Ronald Hordijk <968907+RonaldHordijk@users.noreply.github.com>
2019-03-08 10:17:03 +0100

    Initial utils projects

---
+++ DemoSquared.Utils/Loader.cs
@@ -0,0 +1,24 @@
+﻿using DemoSquared.Model;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace DemoSquared.Utils
+{
+  static public class Loader
+  {
+    static public gbXML Load(string filename)
+    {
+      var serializer = new XmlSerializer(typeof(gbXML));
+      using (var file = File.OpenText(filename))
+      {
+        return serializer.Deserialize(file) as gbXML;
+      }
+    }
+  }
+}
