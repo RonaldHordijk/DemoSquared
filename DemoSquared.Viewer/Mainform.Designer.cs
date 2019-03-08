aabd13d099ffbf992eadbeeb51e9be5b4946cd09
Ronald Hordijk <968907+RonaldHordijk@users.noreply.github.com>
2019-03-08 10:20:02 +0100

    Add viewer

---
+++ DemoSquared.Viewer/Mainform.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace DemoSquared.Viewer
+{
+  partial class MainForm
+  {
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+      this.btnLoad = new System.Windows.Forms.Button();
+      this.SuspendLayout();
+      // 
+      // btnLoad
+      // 
+      this.btnLoad.Location = new System.Drawing.Point(12, 294);
+      this.btnLoad.Name = "btnLoad";
+      this.btnLoad.Size = new System.Drawing.Size(75, 23);
+      this.btnLoad.TabIndex = 0;
+      this.btnLoad.Text = "Load";
+      this.btnLoad.UseVisualStyleBackColor = true;
+      // 
+      // MainForm
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(600, 366);
+      this.Controls.Add(this.btnLoad);
+      this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+      this.Name = "MainForm";
+      this.Text = "DemoViewer";
+      this.Load += new System.EventHandler(this.MainForm_Load);
+      this.ResumeLayout(false);
+
+    }
+
+    #endregion
+
+    private System.ComponentModel.BackgroundWorker backgroundWorker1;
+    private System.Windows.Forms.Button btnLoad;
+  }
+}
+
---
+++ DemoSquared.Viewer/Mainform.cs
@@ -0,0 +1,41 @@
+﻿using DemoSquared.Model;
+using DemoSquared.Utils;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DemoSquared.Viewer
+{
+  public partial class MainForm : Form
+  {
+    private gbXML _building;
+
+    public MainForm()
+    {
+      InitializeComponent();
+    }
+
+    private void MainForm_Load(object sender, EventArgs e)
+    {
+      var filename = @"C:\Code\DemoSquared\Data\OfficeBuilding.xml";
+
+      try
+      {
+        _building = Loader.Load(filename);
+      }
+      catch (Exception ex)
+      {
+        MessageBox.Show($"Unable to load file: {filename}.\n error: {ex.Message}",
+            "Loading failed",
+            MessageBoxButtons.OK,
+            MessageBoxIcon.Error);
+      }
+    }
+  }
+}
