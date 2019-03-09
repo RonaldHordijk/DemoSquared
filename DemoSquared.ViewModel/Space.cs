6e13f2ffb3f8834e7a74bb9a14db1bcb21625205
Ronald Hordijk <968907+RonaldHordijk@users.noreply.github.com>
2019-03-09 10:03:08 +0100

    Add viewModel

---
+++ DemoSquared.ViewModel/Space.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoSquared.ViewModel
+{
+  public class Space
+  {
+    public string Name { get; set; }
+    public List<Polygon> Polygons { get; } = new List<Polygon>();
+
+    public override string ToString() => Name;
+  }
+}
